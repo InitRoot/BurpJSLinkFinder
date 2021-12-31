44343adbe5d560cb65d20764f240e1e13b2b684e
InitRoot <954507+InitRoot@users.noreply.github.com>
2021-12-31 10:36:08 +0200

    v2
    several new features mostly UI related

--- FransLinkfinder.py
+++ FransLinkfinder.py
@@ -1,7 +1,7 @@
 #
 #  BurpLinkFinder - Find links within JS files.
 #
-#  Copyright (c) 2019 Frans Hendrik Botes
+#  Copyright (c) 2022 Frans Hendrik Botes
 #  Credit to https://github.com/GerbenJavado/LinkFinder for the idea and regex
 #
 from burp import IBurpExtender, IScannerCheck, IScanIssue, ITab
@@ -12,6 +12,7 @@ from java.util.regex import Matcher, Pattern
 import binascii
 import base64
 import re
+from os import path
 from javax import swing
 from java.awt import Font, Color
 from threading import Thread
@@ -20,6 +21,22 @@ from java.awt import EventQueue
 from java.lang import Runnable
 from thread import start_new_thread
 from javax.swing import JFileChooser
+from javax.swing import JScrollPane
+from javax.swing import JSplitPane
+from javax.swing import JTabbedPane
+from javax.swing import JTable
+from javax.swing import JPanel
+from javax.swing import JLabel
+from javax.swing.event import DocumentListener
+from javax.swing import JCheckBox
+from javax.swing import SwingUtilities
+from javax.swing import JTextField
+from javax.swing.table import AbstractTableModel
+import urlparse,threading
+try: 
+    import queue
+except ImportError:
+    import Queue as queue
 
 # Using the Runnable class for thread-safety with Swing
 class Run(Runnable):
@@ -30,50 +47,61 @@ class Run(Runnable):
         self.runner()
 
 # Needed params
-
-JSExclusionList = ['jquery', 'google-analytics','gpt.js']
+JSExclusionList = ['jquery', 'google-analytics','gpt.js','modernizr','gtm','fbevents']
 
 class BurpExtender(IBurpExtender, IScannerCheck, ITab):
     def registerExtenderCallbacks(self, callbacks):
         self.callbacks = callbacks
         self.helpers = callbacks.getHelpers()
-        callbacks.setExtensionName("BurpJSLinkFinder")
-
-        callbacks.issueAlert("BurpJSLinkFinder Passive Scanner enabled")
-
+        callbacks.setExtensionName("BurpJSLinkFinderv2")
+        callbacks.issueAlert("BurpJSLinkFinderv2 Passive Scanner enabled")
         stdout = PrintWriter(callbacks.getStdout(), True)
         stderr = PrintWriter(callbacks.getStderr(), True)
         callbacks.registerScannerCheck(self)
+        self.threads = []
         self.initUI()
-        self.callbacks.addSuiteTab(self)
+        # customize our UI components
+        callbacks.customizeUiComponent(self._splitpane)
+        callbacks.customizeUiComponent(self._splitpane2)
+        callbacks.customizeUiComponent(self.logPane)
+        callbacks.customizeUiComponent(self.filesPane)
+        callbacks.customizeUiComponent(self.mapPane)
+        callbacks.customizeUiComponent(self._parentPane)
+        callbacks.customizeUiComponent(self._parentPane)
+        callbacks.customizeUiComponent(self._parentPane)
+        # add the custom tab to Burp's UI
+        callbacks.addSuiteTab(self)
         
-        print ("Burp JS LinkFinder loaded.")
-        print ("Copyright (c) 2019 Frans Hendrik Botes")
-        self.outputTxtArea.setText("Burp JS LinkFinder loaded." + "\n" + "Copyright (c) 2019 Frans Hendrik Botes" + "\n")
+        print ("BurpJS LinkFinder v2 loaded.")
+        print ("Copyright (c) 2022 Frans Hendrik Botes")
+        self.outputTxtArea.setText("BurpJS LinkFinder loaded." + "\n" + "Copyright (c) 2022 Frans Hendrik Botes" + "\n")
 
     def initUI(self):
-        self.tab = swing.JPanel()
-
-        # UI for Output
+        self._parentPane = JTabbedPane()
+        # The main split pane for the components
+        self._splitpane = JSplitPane(JSplitPane.HORIZONTAL_SPLIT)
+        self._splitpane.setDividerLocation(800)
+        # The split pane for the mapping and filenames
+        self._splitpane2 = JSplitPane(JSplitPane.VERTICAL_SPLIT)
+        self._splitpane2.setDividerLocation(300)
+        # UI for Log Output
+        self.logPanel = swing.JPanel()
         self.outputLabel = swing.JLabel("LinkFinder Log:")
-        self.outputLabel.setFont(Font("Tahoma", Font.BOLD, 14))
+        self.outputLabel.setFont(Font("Tahoma", Font.BOLD, 12))
         self.outputLabel.setForeground(Color(255,102,52))
         self.logPane = swing.JScrollPane()
         self.outputTxtArea = swing.JTextArea()
-        self.outputTxtArea.setFont(Font("Consolas", Font.PLAIN, 12))
+        self.outputTxtArea.setFont(Font("Consolas", Font.PLAIN, 10))
         self.outputTxtArea.setLineWrap(True)
         self.logPane.setViewportView(self.outputTxtArea)
-        self.clearBtn = swing.JButton("Clear Log", actionPerformed=self.clearLog)
-        self.exportBtn = swing.JButton("Export Log", actionPerformed=self.exportLog)
+        self.clearBtn = swing.JButton("Clear", actionPerformed=self.clearLog)
+        self.exportBtn = swing.JButton("Export", actionPerformed=self.exportLog)
         self.parentFrm = swing.JFileChooser()
-
-
-
         # Layout
-        layout = swing.GroupLayout(self.tab)
+        layout = swing.GroupLayout(self.logPanel)
         layout.setAutoCreateGaps(True)
         layout.setAutoCreateContainerGaps(True)
-        self.tab.setLayout(layout)
+        self.logPanel.setLayout(layout)
       
         layout.setHorizontalGroup(
             layout.createParallelGroup()
@@ -86,7 +114,6 @@ class BurpExtender(IBurpExtender, IScannerCheck, ITab):
                 )
             )
         )
-        
         layout.setVerticalGroup(
             layout.createParallelGroup()
             .addGroup(layout.createParallelGroup()
@@ -99,25 +126,109 @@ class BurpExtender(IBurpExtender, IScannerCheck, ITab):
             )
         )
 
+        # UI for Filenames Pane
+        self.filePanel = swing.JPanel()
+        self.fileNamesLabel = swing.JLabel("Filenames:")
+        self.fileNamesLabel.setFont(Font("Tahoma", Font.BOLD, 12))
+        self.fileNamesLabel.setForeground(Color(255,102,52))
+        self.filesPane = swing.JScrollPane()
+        self.filesTxtArea = swing.JTextArea()
+        self.filesTxtArea.setFont(Font("Consolas", Font.PLAIN, 10))
+        self.filesTxtArea.setLineWrap(True)
+        self.filesPane.setViewportView(self.filesTxtArea)
+        self.clearFilesBtn = swing.JButton("Clear", actionPerformed=self.clearFilseLog)
+
+        # Layout
+        layoutf = swing.GroupLayout(self.filePanel)
+        layoutf.setAutoCreateGaps(True)
+        layoutf.setAutoCreateContainerGaps(True)
+        self.filePanel.setLayout(layoutf)
+      
+        layoutf.setHorizontalGroup(
+            layoutf.createParallelGroup()
+            .addGroup(layoutf.createSequentialGroup()
+                .addGroup(layoutf.createParallelGroup()
+                    .addComponent(self.fileNamesLabel)
+                    .addComponent(self.filesPane)
+                    .addComponent(self.clearFilesBtn)
+                )
+            )
+        )
+        layoutf.setVerticalGroup(
+            layoutf.createParallelGroup()
+            .addGroup(layoutf.createParallelGroup()
+                .addGroup(layoutf.createSequentialGroup()
+                    .addComponent(self.fileNamesLabel)
+                    .addComponent(self.filesPane)
+                    .addComponent(self.clearFilesBtn)
+                )
+            )
+        )
+
+        # UI for Mapped Pane
+        self.mapPanel = swing.JPanel()
+        self.mapLabel = swing.JLabel("Mapped:")
+        self.mapLabel.setFont(Font("Tahoma", Font.BOLD, 12))
+        self.mapLabel.setForeground(Color(255,102,52))
+        self.mapPane = swing.JScrollPane()
+        self.mapTxtArea = swing.JTextArea()
+        self.mapTxtArea.setFont(Font("Consolas", Font.PLAIN, 10))
+        self.mapTxtArea.setLineWrap(True)
+        self.mapPane.setViewportView(self.mapTxtArea)
+        self.clearMapBtn = swing.JButton("Clear", actionPerformed=self.clearMAPLog)
+        self.mapMapBtn = swing.JButton("Map", actionPerformed=self.mapMaps)
+        # Layout
+        layoutm = swing.GroupLayout(self.mapPanel)
+        layoutm.setAutoCreateGaps(True)
+        layoutm.setAutoCreateContainerGaps(True)
+        self.mapPanel.setLayout(layoutm)
+      
+        layoutm.setHorizontalGroup(
+            layoutm.createParallelGroup()
+            .addGroup(layoutm.createSequentialGroup()
+                .addGroup(layoutm.createParallelGroup()
+                    .addComponent(self.mapLabel)
+                    .addComponent(self.mapPane)
+                    .addComponent(self.clearMapBtn)
+                    .addComponent(self.mapMapBtn)                   
+                )
+            )
+        )
+        layoutm.setVerticalGroup(
+            layoutm.createParallelGroup()
+            .addGroup(layoutm.createParallelGroup()
+                .addGroup(layoutm.createSequentialGroup()
+                    .addComponent(self.mapLabel)
+                    .addComponent(self.mapPane)
+                    .addComponent(self.clearMapBtn)
+                    .addComponent(self.mapMapBtn)
+                )
+            )
+        )
+
+        #Set up all the panes
+        self._splitpane.setLeftComponent(self.logPanel)
+        self._splitpane2.setTopComponent(self.filePanel)
+        self._splitpane2.setBottomComponent(self.mapPanel)
+        self._splitpane.setRightComponent(self._splitpane2)
+        self._parentPane.addTab("Main", self._splitpane)
     def getTabCaption(self):
         return "BurpJSLinkFinder"
-
     def getUiComponent(self):
-        return self.tab
-
+        return self._parentPane
     def clearLog(self, event):
-          self.outputTxtArea.setText("Burp JS LinkFinder loaded." + "\n" + "Copyright (c) 2019 Frans Hendrik Botes" + "\n" )
-
+          self.outputTxtArea.setText("BurpJS LinkFinder loaded." + "\n" + "Copyright (c) 2022 Frans Hendrik Botes" + "\n" )
+    def clearFilseLog(self, event):
+          self.filesTxtArea.setText("")
+    def clearMAPLog(self, event):
+          self.mapTxtArea.setText("")      
     def exportLog(self, event):
         chooseFile = JFileChooser()
         ret = chooseFile.showDialog(self.logPane, "Choose file")
         filename = chooseFile.getSelectedFile().getCanonicalPath()
         print("\n" + "Export to : " + filename)
         open(filename, 'w', 0).write(self.outputTxtArea.text)
-
-    
-    def doPassiveScan(self, ihrr):
-        
+    def doPassiveScan(self, ihrr):      
         try:
             urlReq = ihrr.getUrl()
             testString = str(urlReq)
@@ -131,23 +242,117 @@ class BurpExtender(IBurpExtender, IScannerCheck, ITab):
                     self.outputTxtArea.append("\n" + "[+] Valid URL found: " + str(urlReq))
                     issueText = linkA.analyseURL()
                     for counter, issueText in enumerate(issueText):
-                            #print("TEST Value returned SUCCESS")
-                            self.outputTxtArea.append("\n" + "\t" + str(counter)+' - ' +issueText['link'])   
+                            self.outputTxtArea.append("\n" + "\t" + issueText['link'])
+                            if linkA.valcheckUrl(issueText['link']):
+                                fullURL = urlparse.urljoin(str(urlReq), '/') + issueText['link']
+                                
+                                self.mapTxtArea.append("\n" + fullURL)
+                            filNam = os.path.basename(issueText['link'])
+                            if linkA.isNotBlank((filNam)):
+                                try:
+                                    filNam = filNam[ 0 : filNam.index("?")]
+                                except:
+                                    filNam = filNam
 
+                                if (linkA.checkValidFile(filNam)) and (filNam not in self.filesTxtArea.text):
+                                    self.filesTxtArea.append("\n" + filNam)
+                            
                     issues = ArrayList()
                     issues.add(SRI(ihrr, self.helpers))
                     return issues
         except UnicodeEncodeError:
             print ("Error in URL decode.")
         return None
-
-
     def consolidateDuplicateIssues(self, isb, isa):
         return -1
-
     def extensionUnloaded(self):
-        print "Burp JS LinkFinder unloaded"
+        print "BurpJS LinkFinder v2 unloaded"
         return
+
+    def mapMaps(self,event):
+        self.q = queue.Queue()
+        get_all_urls = self.mapTxtArea.getText()
+        urls_list = list(set(get_all_urls.split('\n')))
+
+        for url in urls_list:
+            url = url.rstrip()
+            self.q.put(url)
+
+        for j in range(10):
+            t = threading.Thread(target=self.ProcessQueue)
+            self.threads.append(t)
+            t.start()
+
+    def ProcessQueue(self):
+        while not self.q.empty():
+            each_url = self.q.get()
+            self.ProcessURL(each_url)
+            self.q.task_done()
+            self.mapTxtArea.setText("")        
+
+    def URL_SPLITTER(self,url):
+        URL_SPLIT = str(url).split("://",1)
+        URL_PROTOCAL = URL_SPLIT[0]
+        if URL_PROTOCAL == 'https':
+            URL_PORT = 443
+        elif URL_PROTOCAL == 'http':
+            URL_PORT = 80
+        else:
+            URL_PORT = 443
+        URL_HOSTNAME = URL_SPLIT[1].split('/',1)[0].split('?',1)[0]
+        if ':' in URL_HOSTNAME:
+            URL_HOSTNAME_FOR_SPLIT = URL_HOSTNAME
+            URL_HOSTNAME = URL_HOSTNAME_FOR_SPLIT.split(':')[0]
+            URL_PORT = int(URL_HOSTNAME_FOR_SPLIT.split(':')[1])
+        URL_HOST_FULL = URL_PROTOCAL+"://"+URL_HOSTNAME
+        try:
+            URL_HOST_SERVICE = self.helpers.buildHttpService(URL_HOSTNAME,URL_PORT,URL_PROTOCAL)
+        except java.lang.IllegalArgumentException:
+            print("EXCEPTION BECAUSE HTTPSERVICE VALUES IS INVALID : {} : ".format(url))
+            print("EXCEPTION VALUES ARE :",URL_HOSTNAME,URL_PORT,URL_PROTOCAL)
+        return URL_SPLIT,URL_PROTOCAL,URL_HOSTNAME,URL_PORT,URL_HOST_FULL,URL_HOST_SERVICE
+
+    def ProcessURL(self,url):
+        #print(url)
+        if url.startswith('http://') or url.startswith('https://'):
+            URL_SPLIT,URL_PROTOCAL,URL_HOSTNAME,URL_PORT,URL_HOST_FULL,URL_HOST_SERVICE = self.URL_SPLITTER(url)
+            try:
+                HEADERS = ["GET /"+str(URL_SPLIT[1].split('/',1)[1])+" HTTP/1.1",'Host: '+str(URL_HOSTNAME)]
+            except:
+                novar = 1
+                #print("URL EXCEPTION IN HEADERS : {} : {}".format(url,URL_SPLIT))
+            msg = self.helpers.buildHttpMessage(HEADERS,None)
+            resp = self.callbacks.makeHttpRequest(URL_HOST_SERVICE,msg)
+            if resp.getResponse() != None:
+                resp_analyze = self.helpers.analyzeResponse(resp.getResponse())
+                self.callbacks.addToSiteMap(resp)
+                resp_heads = resp_analyze.getHeaders()
+                if '301' in resp_heads[0] or 'Moved' in resp_heads[1] or '307' in resp_heads[0] or '302' in resp_heads[0]:
+                    for each_head in resp_heads:
+                        if each_head.startswith('Location:') or each_head.startswith('location:'):
+                            location_value = each_head.split(":",1)[1].strip(' ')
+                            if location_value.startswith('http'):
+                                URL_SPLIT,URL_PROTOCAL,URL_HOSTNAME,URL_PORT,URL_HOST_FULL,URL_HOST_SERVICE = self.URL_SPLITTER(location_value)
+                                try:
+                                    HEADERS = ["GET /"+str(URL_SPLIT[1].split('/',1)[1])+" HTTP/1.1",'Host: '+str(URL_HOSTNAME)]
+                                except:
+                                    #print("URL EXCEPTION IN REDIRECTION HEADERS : {}".format(URL_SPLIT))
+                                    return False
+                                if self.HEADERS:
+                                    for each_header in self.HEADERS:
+                                        if each_header not in HEADERS:
+                                            HEADERS.append(each_header)
+                            elif location_value.startswith('/'):
+                                HEADERS = ["GET "+str(location_value)+" HTTP/1.1",'Host: '+str(URL_HOSTNAME)]
+                                if self.HEADERS:
+                                    for each_header in self.HEADERS:
+                                        if each_header not in HEADERS:
+                                            HEADERS.append(each_header)
+                            else:
+                                pass
+                            msg = self.helpers.buildHttpMessage(HEADERS,None)
+                            resp = self.callbacks.makeHttpRequest(URL_HOST_SERVICE,msg)            
+                            self.callbacks.addToSiteMap(resp)
 
 class linkAnalyse():
     
@@ -216,27 +421,59 @@ class linkAnalyse():
             else:
                 filtered_items.append(item)
         return filtered_items
-
     # Potential for use in the future...
     def threadAnalysis(self):
         thread = Thread(target=self.analyseURL(), args=(session,))
         thread.daemon = True
         thread.start()
 
-    def analyseURL(self):
-        
+    def analyseURL(self):      
         endpoints = ""
-        #print("TEST AnalyseURL #1")
         mime_type=self.helpers.analyzeResponse(self.reqres.getResponse()).getStatedMimeType()
         if mime_type.lower() == 'script':
                 url = self.reqres.getUrl()
                 encoded_resp=binascii.b2a_base64(self.reqres.getResponse())
                 decoded_resp=base64.b64decode(encoded_resp)
                 endpoints=self.parser_file(decoded_resp, self.regex_str)
-                #print("TEST AnalyseURL #2")
                 return endpoints
         return endpoints
 
+    def checkValidFile(self,fileNam):
+        regexFile = """^[a-zA-Z0-9](?:[a-zA-Z0-9 ._-]*[a-zA-Z0-9])?.[a-zA-Z0-9_-][.].*"""
+        try:
+            if fileNam and fileNam.strip():
+                return bool(re.search(regexFile, fileNam))
+            
+        except:
+
+
+            return False
+       
+    def isNotBlank(self,myString):
+        try:
+            if myString and myString.strip():
+                #myString is not None AND myString is not empty or blank
+                return True
+            #myString is None OR myString is empty or blank
+        except:
+
+
+            return False
+
+    def valcheckUrl(self,myString):
+        #Checks if the extracted URL is a full URL or if already in the mapped list
+        #print("Checking URL: " + myString)
+        try:
+            if ("http" in myString) or (myString in mapTxtArea.text):
+                #print("Found HTTP in URL: " + myString)
+                return False
+            
+        except Exception as e:
+            print(myString + "\t" + str(e))
+            return True
+
+        #print("Returning Default: " + myString)
+        return True
 
 class SRI(IScanIssue,ITab):
     def __init__(self, reqres, helpers):
